9c11b88a5ca2b83c2af36353f0ec141fa40b9f3e
ah3243 <fUbzz=30E>
2018-04-02 20:18:38 +0100

    Pi Vid capture and saving working.

--- UtilCode/PiVidCap.py
+++ UtilCode/PiVidCap.py
@@ -14,21 +14,19 @@ camera.framerate = 32
 rawCapture = PiRGBArray(camera, size = frameSize)
 
 # Define saving codec and create writing object
-# fourCC = cv2.VideoWriter_fourcc(*'mp4v')
-# out = cv2.VideoWriter("piOutput.avi", fourCC, 20.0, frameSize)
+fourCC = cv2.VideoWriter_fourcc(*'MJPG')
+out = cv2.VideoWriter("piOutput.avi", fourCC, 20.0, frameSize)
 
 time.sleep(0.1)
 
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     image = frame.array
 
-    # Resize frame to output size
-    image = cv2.resize(image, frameSize, interpolation = cv2.INTER_AREA)
-    # Convert frame to grayscale
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # # Resize frame to output size
+    # image = cv2.resize(image, frameSize, interpolation = cv2.INTER_AREA)
 
-    # # Add frame to writing object
-    # out.write(image)
+    # Add frame to writing object
+    out.write(image)
 
     cv2.imshow("img", image)
     key = cv2.waitKey(1) & 0xFF
@@ -38,5 +36,6 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     if key == ord("q"):
         break
 
-# out.release()
+camera.close()
+out.release()
 cv2.destroyAllWindows()
